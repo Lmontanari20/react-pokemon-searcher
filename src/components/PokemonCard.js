2e25a597a8cb505f6774919ac490d634aa381aae
Lucas Montanari <Lmonty2011@gmail.com>
2021-02-08 16:30:04 -0800

    commit tree 7611846097cb1bd645612358fd149c5546015b24

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -2,20 +2,37 @@ import React from 'react'
 import { Card } from 'semantic-ui-react'
 
 class PokemonCard extends React.Component {
+  constructor(props) {
+    super()
+    this.state = {
+      clicked: false,
+      display: props.front
+    }
+  }
+
+  onClick = () => {
+    this.setState(previousState => {
+      return {
+        clicked: !previousState.clicked,
+        display: previousState === true ? this.props.front : this.props.back
+      }
+    })
+  }
+
   render() {
     return (
       <Card>
         <div>
           <div className="image">
-            <img alt="oh no!" />
+            <img onClick={this.onClick} src={this.state.display}alt="oh no!" />
           </div>
           <div className="content">
-            <div className="header">POKEMON NAME HERE</div>
+            <div className="header">{this.props.name}</div>
           </div>
           <div className="extra content">
             <span>
               <i className="icon heartbeat red" />
-              POKEMON HP HERE hp
+              {this.props.hp}
             </span>
           </div>
         </div>
--- src/components/PokemonCollection.js
+++ src/components/PokemonCollection.js
@@ -3,10 +3,19 @@ import PokemonCard from './PokemonCard'
 import { Card } from 'semantic-ui-react'
 
 class PokemonCollection extends React.Component {
+
+
+  renderPokemon = pokemon => {
+    return pokemon.map((poke) => {
+      return <PokemonCard key={poke.id} name={poke.name} hp={poke.hp} front={poke.sprites.front} back={poke.sprites.back} />
+    })
+  }
+
+
   render() {
     return (
       <Card.Group itemsPerRow={6}>
-        <h1>Hello From Pokemon Collection</h1>
+        {this.renderPokemon(this.props.filteredPokemon)}
       </Card.Group>
     )
   }
